10bf1ee299b766683739a73f1080a03fa8b64eab
laiaxarau <laiaxarau@gmail.com>
2024-05-28 21:23:10 +0200

    Projectes

--- 02-tic-tac-toe/src/components/Square.jsx
+++ 02-tic-tac-toe/src/components/Square.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+import { useCallback } from "react"
+import { useEffect } from "react"
+
 export const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
-
+  
+const [numero,setNumero]=useState(0)
+//explicació xevi sobre useCallback
+  const sayHello=useCallback(()=>console.log(numero),[numero])
+  
   const handleClick = () => {
     updateBoard(index)
+    sayHello()
+    setNumero(numero+1)
+    console.log(numero)
   }
   return (
     <div onClick={handleClick} className={className}>
